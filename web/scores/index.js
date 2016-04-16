446e34a81019e92e874874b7ef7450130e9b9243
Drew Anderson <drew@onlycentral.com>
2016-04-16 15:07:59 +1000

    scores initial draft

--- web/scores/index.js
+++ web/scores/index.js
@@ -1,5 +1,35 @@
+var router = require('express').Router();
+var jwt = require('jsonwebtoken');
 
+var jwtPassword = "ludum35-aa950054-038f-11e6-a953-9f8fa3b1b12c";
 
-module.exports = function(request, response) {
+router.get('/', function handleRoot(request, response) {
+    var data = {
+        score: 500,
+        name: "someone",
+    };
 
-};
+    var token = jwt.sign(data, jwtPassword);
+
+    response.render('scores/index', {
+        token: token,
+    });
+});
+
+router.get('/:token', function handleScore(request, response) {
+    var token = request.token;
+
+    jwt.verify(request.params.token, jwtPassword, function(err, decoded) {
+        if (err) {
+            response.status(404);
+            response.render('scores/scoreError');
+            return;
+        }
+
+        response.render('scores/scoreSuccess', {
+            token: decoded,
+        });
+    });
+});
+
+module.exports = router;
