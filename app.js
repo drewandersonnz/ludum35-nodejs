8cc72c3e66c4933978c5e2b84ca33d4ce882f6b7
Drew Anderson <drew@onlycentral.com>
2016-04-16 13:25:08 +1000

    Must respond HTTP 200 to /health for openshift

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.get('/', function(request, response) {
     });
 });
 
+// Must respond HTTP 200 to /health for openshift
+app.get('/health', function(request, response) {
+    response.send(200);
+});
+
 app.listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || 'localhost', function () {
     console.log(`Application worker ${process.pid} started...`);
 });
