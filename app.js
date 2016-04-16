f5c99129f40bc4a89d76f92c4a5d78f49076cf35
Drew Anderson <drew@onlycentral.com>
2016-04-16 14:07:35 +1000

    re-arranging structure; adding scores;

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 var express = require('express');
 
 var app = express();
-app.set('views', './http_views');
+app.set('views', './web');
 app.set('view engine', 'jade');
 
+// Must respond HTTP 200 to /health for openshift
+app.get('/health', function(request, response) {
+    response.send(200);
+});
+
+// Homepage
 app.get('/', function(request, response) {
-    response.render('home', {
+    response.render('index', {
         raw: {
             request: request,
         },
     });
 });
 
-// Must respond HTTP 200 to /health for openshift
-app.get('/health', function(request, response) {
-    response.send(200);
-});
+var web = require('./web');
+
+// Scores
+app.use('/scores', web.scores);
 
 app.listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || 'localhost', function () {
     console.log(`Application worker ${process.pid} started...`);
